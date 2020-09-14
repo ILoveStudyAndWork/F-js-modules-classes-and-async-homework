f1b54363b0945dc96990eb737ef9b00528b8acc4
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-14 19:28:19 +0800

    [fengyi_ling] feat:complete practice3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = async () => {
+    const url = 'https://v1.jinrishici.com/all.json';
+    let result = [];
+    const data = await fetch(url)
+        .then(response => response.json())
+    result.push(data.origin);
+    result.push(data.author);
+    result.push(data.content);
+    return result;
+
+
+
+}
